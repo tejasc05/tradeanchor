1273cf9dbe3d819154faa86c04cbf732a7856bf3
Tejas Chaudhari <tejas03c@gmail.com>
2026-06-28 00:15:47 -0400

    refactor stats logic into separate folder for reusability

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,7 @@ def health():
 @app.post("/evaluate")
 def evaluate(req: EvaluationRequest):
     evaluations = evaluate_trades(req.trades, req.strategy)
+    
     summary = calculate_statistics(evaluations)
 
     return {
