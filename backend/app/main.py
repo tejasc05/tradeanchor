c075a215cb3e83d58e379bee538288adfedeb4c8
Jacob Garrett <jacobgarrett@Jacobs-MBP-100.hsd1.fl.comcast.net>
2026-06-27 23:58:16 -0400

    Extract trade statistics into resuable module

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.models import EvaluationRequest
 from app.evaluator import evaluate_trades
+from app.statistics import calculate_statistics
+
 
 # creating and naming the api
 app = FastAPI(title="TradeAnchor API")
@@ -12,34 +14,10 @@ def health():
 @app.post("/evaluate")
 def evaluate(req: EvaluationRequest):
     evaluations = evaluate_trades(req.trades, req.strategy)
-    
-    total_trades = len(evaluations)
-    followed_count = sum(1 for evaluation in evaluations if evaluation.followed_plan)
-
-    if total_trades == 0:
-        anchor_score = 0
-    else:
-        anchor_score=round((followed_count/total_trades)*100, 2)
-
-    in_strategy_pnl = sum(
-        evaluation.trade.realized_pnl
-        for evaluation in evaluations
-        if evaluation.followed_plan
-    )
-
-    out_of_strategy_pnl = sum(
-        evaluation.trade.realized_pnl
-        for evaluation in evaluations
-        if not evaluation.followed_plan
-    )
+    summary = calculate_statistics(evaluations)
 
     return {
-        "anchor_score": anchor_score,
-        "total_trades": total_trades,
-        "followed_plan": followed_count,
-        "broke_plan": total_trades - followed_count,
-        "in_strategy_pnl": in_strategy_pnl,
-        "out_of_strategy_pnl": out_of_strategy_pnl,
+       **summary
         "evaluations": evaluations,
     }
 
---
+++ backend/app/statistics.py
@@ -0,0 +1,34 @@
+from app.models import TradeEvaluation
+
+def calculate_statistics(evaluations: List[TradeEvaluation]) -> dict:
+    evaluations = evaluate_trades(req.trades, req.strategy)
+    
+    total_trades = len(evaluations)
+    followed_count = sum(1 for evaluation in evaluations if evaluation.followed_plan)
+
+    if total_trades == 0:
+        anchor_score = 0
+    else:
+        anchor_score=round((followed_count/total_trades)*100, 2)
+
+    in_strategy_pnl = sum(
+        evaluation.trade.realized_pnl
+        for evaluation in evaluations
+        if evaluation.followed_plan
+    )
+
+    out_of_strategy_pnl = sum(
+        evaluation.trade.realized_pnl
+        for evaluation in evaluations
+        if not evaluation.followed_plan
+    )
+
+    return {
+        "anchor_score": anchor_score,
+        "total_trades": total_trades,
+        "followed_plan": followed_count,
+        "broke_plan": total_trades - followed_count,
+        "in_strategy_pnl": in_strategy_pnl,
+        "out_of_strategy_pnl": out_of_strategy_pnl,
+        "evaluations": evaluations,
+    }
